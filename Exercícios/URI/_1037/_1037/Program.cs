2400c79df1534cea01bdcabcc076f08fdce0a385
Daniel Andrade Silva <daniel.andrade@hbsis.com.br>
2020-01-30 09:55:04 -0300

    _1037 aceita

--- Exercícios/URI/_1037/_1037/Program.cs
+++ Exercícios/URI/_1037/_1037/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace _1037
 {
@@ -10,6 +7,20 @@ namespace _1037
     {
         static void Main(string[] args)
         {
+            double entrada = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            string saida;
+
+            if (0 <= entrada && entrada <= 25.00)
+                saida = "Intervalo [0,25]";
+            else if (25.00 < entrada && entrada <= 50.00)
+                saida = "Intervalo (25,50]";
+            else if (50.00 < entrada && entrada <= 75.00)
+                saida = "Intervalo (50,75]";
+            else if (75.00 < entrada && entrada <= 100.00)
+                saida = "Intervalo (75,100]";
+            else
+                saida = "Fora de intervalo";
+            Console.WriteLine(saida);
         }
     }
 }
