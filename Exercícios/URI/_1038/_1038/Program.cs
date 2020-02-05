632874d4b43acd3a4c24ae56dacf75cc381cac78
Daniel Andrade Silva <daniel.andrade@hbsis.com.br>
2020-02-05 10:26:42 -0300

    _1038

---
+++ Exercícios/URI/_1038/_1038/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1038
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] entrada = Console.ReadLine().Split(' ');
+            int codigo = int.Parse(entrada[0]);
+            int quantidade = int.Parse(entrada[1]);
+            double valor;
+            switch (codigo) 
+            {
+                case 1:
+                    valor = 4.00 * quantidade;
+                    break;
+                case 2:
+                    valor = 4.50 * quantidade;
+                    break;
+                case 3:
+                    valor = 5.00 * quantidade;
+                    break;
+                case 4:
+                    valor = 2.00 * quantidade;
+                    break;
+                case 5:
+                    valor = 1.50 * quantidade;
+                    break;
+                default:
+                    valor = 0;
+                    break;
+            } 
+            Console.WriteLine($"Total: R$ {valor.ToString("F2",CultureInfo.InvariantCulture)}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TrabalhandoComArquivosDeTexto/TrabalhandoComArquivosDeTexto/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TrabalhandoComArquivosDeTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declarando a variavel do tipo StreamWriter para
+            //abrir ou criar um arquivo para escrita
+            StreamWriter x;
+
+            //Colocando o caminho físico e o nome do arquivo a ser criado
+            //finalizando com .txt
+            string CaminhoNome = "C:\\Users\\daniel.andrade\\Desktop\\arq01.txt";
+
+            //utilizando o método para criar um arquivo texto
+            //e associando o caminho e nome ao metodo
+            x = File.CreateText(CaminhoNome);
+
+            //aqui, exemplo de escrever no arquivo texto
+            //como se fossemos criar um recibo de pagamento
+
+            //escrevendo o título
+            x.WriteLine("Termo de Uso");
+            //pulando linha sem escrita
+            x.WriteLine();
+            //escrevendo o conteudo do termo
+            x.WriteLine("Art. 1º - ACEITAÇÃO");
+            x.WriteLine("Você reconhece que analisou e aceitou as condições de uso. Leia-as atentamente " +
+                "pois o uso deste aplicativo significa que você aceitou todos os termos e concorda em " +
+                "cumpri-los.Se você, usuário, for menor de idade oudeclarado incapaz em quaisquer aspectos, " +
+                "precisará da permissão de seus pais ou responsáveis que também deverão concordar com estes" +
+                " mesmos termos e condições.");
+            x.WriteLine("Art. 2º - LIMITAÇÕES");
+            x.WriteLine("O aplicativo deverá ser utilizado por você, usuário. A venda, transferência, modificação," +
+                " engenharia reversa ou distribuição bem como a cópia de textos, imagens ou quaisquer partes " +
+                "nele contido é expressamente proibida.");
+            x.WriteLine($"Blumenau,{DateTime.Now}");
+            //fechando o arquivo texto com o método .Close()
+            x.Close();
+        }
+    }
+}
