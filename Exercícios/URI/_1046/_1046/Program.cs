c3ab4e879d0cc98f38e5afab43b45ddde51c1c62
Daniel Andrade Silva <daniel.andrade@hbsis.com.br>
2020-02-11 16:52:55 -0300

    _1046; _1047 terminar

---
+++ Exercícios/URI/_1046/_1046/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] entrada = Console.ReadLine().Split(' ');
+            int a = int.Parse(entrada[0]);
+            int b = int.Parse(entrada[1]);
+            int saldo;
+            if (a >= b){
+                saldo = (b - a) + 24;
+            }
+            else {
+                saldo = b - a;
+            }
+            Console.WriteLine("O JOGO DUROU {0} HORA(S)", saldo);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercícios/URI/_1047/_1047/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1047
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, hora, min;
+            string[] entrada = Console.ReadLine().Split(' ');
+            a = int.Parse(entrada[0]);
+            b = int.Parse(entrada[1]);
+            c = int.Parse(entrada[2]);
+            d = int.Parse(entrada[3]);
+            
+            if(a <= c && b > d)
+
+            Console.WriteLine($"O JOGO DUROU {hora} HORA(S) E {min} MINUTO(S)");
+            Console.ReadKey();
+        }
+    }
+}
