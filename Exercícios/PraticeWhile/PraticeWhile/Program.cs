911de17e5a8cd84c8a00b9d68685d8ffe9c213ee
Daniel Andrade Silva <daniel.andrade@hbsis.com.br>
2020-04-02 22:01:19 -0300

    Pratice While

--- Exercícios/PraticeWhile/PraticeWhile/Program.cs
+++ Exercícios/PraticeWhile/PraticeWhile/Program.cs
@@ -10,20 +10,27 @@ namespace PraticeWhile
             /*
              Faça um programa para ler uma número indeterminado de dados,
              contendo cada um, a idade de um indivíduo.O último dado, que 
-             que nãoe entrará nos cálculos, contém um valor de idade nefativa.
-             Calcular e imprimir a idade média deste gripo de indivíduos. Se 
-             for entrado um valor negativo na primeira ves, mostra a mensagem
+             que não entrará nos cálculos, contém um valor de idade negativa.
+             Calcular e imprimir a idade média deste grupo de indivíduos. Se 
+             for entrado um valor negativo na primeira vez, mostra a mensagem
              "impossivel calcular".
              */
-
+            int repeticoes = 0;
+            double somaIdades = 0.0;
             double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
             if (x < 0)
                 Console.WriteLine("impossivel calcular");
             else
             {
-                while (x > 0)
-                { 
+                while (x >= 0)
+                {
+                    somaIdades = somaIdades + x;
+                    repeticoes++;
+                    x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                 }
+                double media = somaIdades / repeticoes;
+                Console.WriteLine(media.ToString("F2"));
             }
             Console.ReadKey();
         }
