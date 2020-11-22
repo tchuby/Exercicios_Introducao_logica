25b87862f6cbaa4430de2782b9aee504629f13b7
Daniel Andrade Silva <daniel.andrade@hbsis.com.br>
2020-11-22 13:47:20 -0300

    Finalizado o curso

---
+++ Exercícios/Vetor_Ex_Resolvido01/Vetor_Ex_Resolvido01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Vetor_Ex_Resolvido01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //imprimir na tela apenas os números negativos escritos na linha
+
+            string[] s = Console.ReadLine().Split(' ');
+            int n = s.Length;
+            int[] vet = new int[n];
+
+            for(int i = 0; i < vet.Length; i++)
+            {
+                vet[i] = int.Parse(s[i]);
+            }
+
+            for (int j = 0; j < vet.Length; j++) 
+            {
+                if(vet[j] < 0)
+                    Console.WriteLine(vet[j]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercícios/Vetor_Ex_Resolvido02/Vetor_Ex_Resolvido02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Vetor_Ex_Resolvido02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 'n' números reais separados por espaço.");
+
+            string[] vet = Console.ReadLine().Split(' ');
+            float[] numberColection = new float[vet.Length];
+            for(int i = 0; i < vet.Length; i++)
+            {
+                numberColection[i] = float.Parse(vet[i]);
+            }
+        }
+    }
+}
